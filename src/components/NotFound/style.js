7a65905f76195726b1c9a10d9e91d27cc1bbd749
kacwojdev <wojnowski.kacper@gmail.com>
2023-01-10 11:58:47 +0100

    rearranged not found component and styled

---
+++ src/components/NotFound/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const NotFoundContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 2rem;
+    font-weight: 700;
+
+    & > a {
+        color: black;
+        font-weight: 400;
+    }
+`
